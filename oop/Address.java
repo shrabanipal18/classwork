becff0120d685ebe5931957f2b339f90d8732dd6
shrabanipal18 <120494577+shrabanipal18@users.noreply.github.com>
2023-01-05 18:05:37 +0530

    Add files via upload

---
+++ oop/Address.java
@@ -0,0 +1,39 @@
+package oop;
+
+public class Address {
+
+		long pincode;
+		String city,country,state;
+		public Address(long pincode,String city,String country,String state) {
+			this.pincode=pincode;
+			this.city=city;
+			this.country=country;
+			this.state=state;
+		}
+		public long getPincode() {
+			return pincode;
+		}
+		public void setPincode(long pincode) {
+			this.pincode=pincode;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city=city;
+		}
+		public String getCountry() {
+			return country;
+		}
+		public void setCountry(String country) {
+			this.country=country;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setPincode(String state) {
+			this.state=state;
+		}
+
+	}
+
---
+++ oop/Aggregation.java
@@ -0,0 +1,34 @@
+package oop;
+class TCS
+{
+	int comId;
+	String comname;
+	Address adrress; //entity
+	public int getComId() {
+		return comId;
+	}
+	public String getComname() {
+		return comname;
+	}
+	public Address getAdrress() {
+		return adrress;
+	}
+	public TCS(int comId,String comname,Address adrress) {
+		this.comId=comId;
+		this.comname=comname;
+		this.adrress=adrress;
+	}
+}
+public class Aggregation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Address add1=new Address(700032,"Kolkata","India","WB");
+		Address add2=new Address(756904,"New York","USA","XYZ");
+		TCS com1=new TCS(101,"TCS",add1);
+		TCS com2=new TCS(101,"TCS BPS",add2);
+		System.out.println("Com_Id: "+com1.getComId()+"\n"+"Company_Name: "+com1.getComname()+"\n"+"Pincode: "+com1.getAdrress().getPincode()+"\n"+"City: "+com1.getAdrress().getCity()+"\n"+"Country: "+com1.getAdrress().getCountry()+"\n"+"State: "+com1.getAdrress().getState());
+
+	}
+
+}
